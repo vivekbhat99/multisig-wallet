68fd3f70de136a8457d4eeca8f2d4404b9e9196d
vivek <vbhat99999@gmail.com>
2020-10-29 18:53:03 +0530

    initialized react

--- wallet/migrations/2_deploy.js
+++ wallet/migrations/2_deploy.js
@@ -2,16 +2,15 @@ const MultiSigWallet = artifacts.require("MultiSig")
 
 //  testnet accounts
 module.exports = function(deployer, network, accounts) {
-  if (network === "main") {
-    return
-  }
+  // if (network === "main") {
+  //   return
+  // }
 
-  console.log("-----------------------------")
-  console.log(accounts)
-  console.log("-----------------------------")
+  // console.log("-----------------------------")
+  // console.log(accounts)
+  // console.log("-----------------------------")
 
   const owners = accounts.slice(0, 3)
   const minimumConformations = 2
-
-  return deployer.deploy(MultiSigWallet, owners, minimumConformations)
+  deployer.deploy(MultiSigWallet, owners, minimumConformations)
 }
