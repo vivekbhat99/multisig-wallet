70637a0aa9d4f712c6e2c976de843f3f97c95679
vivek <vbhat99999@gmail.com>
2020-11-20 22:35:19 +0530

    ui

--- multisig-wallet/src/App.js
+++ multisig-wallet/src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Message, Button } from "semantic-ui-react";
 
 function App() {
+  const account = "0x665599"
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Multi-Signature Wallet</h1>
+        <div>Account: {account}</div>
+
+        <Message warning>Metamask is not connected :/</Message>
+        <Button color = "green">Connect to Metamask</Button>
+
+
       </header>
     </div>
   );
